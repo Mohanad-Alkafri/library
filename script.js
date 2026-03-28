10d314b12fff3d97de66e1105b17dc6421f8c6b6
Klamkh <mohanad.alkafri@gmail.com>
2026-03-28 23:55:27 +0100

    worked on some functions and added shelves and books container

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // foundation of basic functions and variables
-const library = [1,2,3];
+const library = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
 
 function Book(title, author, pages, read){
     this.title = title,
@@ -13,25 +13,81 @@ Book.prototype.status = function () {return `${this.title}, by ${this.author},
             has ${this.pages} pages and I've ${this.read}`
         };
 
-
 function addBookToLibrary(book){
     library.push(book)
 };
 
-const frame = document.querySelector('.frame')
 const container = document.querySelector('.container')
-const shelf = document.createElement('div')
+let shelf = document.createElement('div')
 shelf.className = ('shelf')
 container.appendChild(shelf)
-const shelfBg = document.createElement('div')
+let shelfBg = document.createElement('div')
 shelfBg.className = 'shelfBg'
 container.appendChild(shelfBg)
 
+function newShelf (){
+        shelf = document.createElement('div')
+        shelf.className = ('shelf')
+        container.appendChild(shelf)
+        shelfBg = document.createElement('div')
+        shelfBg.className = 'shelfBg'
+        container.appendChild(shelfBg)
+}
+if(library.length > 10 && library.length <= 20){
+    newShelf()
+}
+if(library.length > 20 && library.length <= 30){
+    newShelf()
+    newShelf()
+}
+
+
+
+
+
+// creat book covers
+for(i = 0; i<library.length; i++){
+    let cover = document.createElement('div')
+    cover.className = 'cover'
+    if(i >= 10 && i<20){
+        shelf = document.querySelector('.container > :nth-child(3)')
+        shelf.appendChild(cover)
+    }
+    else if(i >= 20){
+        shelf = document.querySelector('.container > :nth-child(5)')
+        shelf.appendChild(cover)
+    }
+    else{
+        shelf = document.querySelector('.container :first-child')
+        shelf.appendChild(cover)
+    }
+}
+
+// create info containers in cover
+const authorCon = document.createElement('p')
+authorCon.className = 'authorCon'
+const titleCon = document.createElement('p')
+titleCon.className = 'titleCon'
+const pagesCon = document.createElement('p')
+pagesCon.className = 'pagesCon'
+const readCon = document.createElement('div')
+readCon.className = 'readCon'
 
 // for loop to add library to display
 for(let i=0; i<library.length; ++i){
-    let bookCon = document.createElement('div')
-    bookCon.className = 'bookCon'
-    shelf.appendChild(bookCon)
-    bookCon.textContent = library[i]
-}
+    function showBook () {
+        
+    }
+    if(i >= 10 && i<20){
+        shelf = document.querySelector('.container > :nth-child(3)')
+        showBook()
+    }
+    else if(i >= 20){
+        shelf = document.querySelector('.container > :nth-child(5)')
+        showBook()
+    }
+    else{
+        shelf = document.querySelector('.container :first-child')
+        showBook()
+    }
+    }
